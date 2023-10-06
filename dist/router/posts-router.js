f617eb3a113fe54f5846f83ba57a362cee0d8498
MihPar <9496260@gmail.com>
2023-10-06 14:40:15 +0300

    0101

--- dist/router/posts-router.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.postsRouter = void 0;
-const express_1 = require("express");
-exports.postsRouter = (0, express_1.Router)({});
-exports.postsRouter.get('/', function (req, res) {
-    res.status(200).send('fjwfsdfsdiofsdifsdiofsdiofsdiofsdiofuseiofdiofsdiof');
-});
