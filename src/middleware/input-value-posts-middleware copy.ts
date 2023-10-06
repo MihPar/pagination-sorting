8702be779fbd1d8dcfc4c49604ff7db1eff5b391
MihPar <9496260@gmail.com>
2023-10-06 20:57:56 +0300

    0808

--- src/middleware/input-value-posts-middleware copy.ts
+++ src/middleware/input-value-posts-middleware copy.ts
@@ -1,37 +1,36 @@
-import { blogsRepositories } from './../repositories/blogs-db-repositories';
-import {body} from 'express-validator'
+import { blogsRepositories } from "./../repositories/blogs-db-repositories";
+import { body } from "express-validator";
 
+export const inputPostTitleValidator = body("title")
+  .isString()
+  .notEmpty()
+  .trim()
+  .isLength({ min: 1, max: 30 })
+  .withMessage("Name should be length from 1 to 30 symbols");
 
-export const inputPostTitleValidator = body('title')
-.isString()
-.notEmpty()
-.trim()
-.isLength({min: 1, max: 30})
-.withMessage('Name should be length from 1 to 30 symbols')
+export const inputPostShortDescriptionValidator = body("shortDescription")
+  .isString()
+  .notEmpty()
+  .trim()
+  .isLength({ min: 1, max: 100 })
+  .withMessage("Descriptionme should be length from 1 to 100 symbols");
 
-export const inputPostShortDescriptionValidator = body('shortDescription')
-.isString()
-.notEmpty()
-.trim()
-.isLength({min: 1, max: 100})
-.withMessage('Descriptionme should be length from 1 to 100 symbols')
+export const inputPostContentValidator = body("content")
+  .isString()
+  .notEmpty()
+  .trim()
+  .isLength({ min: 1, max: 1000 })
+  .withMessage("Descriptionme should be length from 1 to 1000 symbols");
 
-export const inputPostContentValidator = body('content')
-.isString()
-.notEmpty()
-.trim()
-.isLength({min: 1, max: 1000})
-.withMessage('Descriptionme should be length from 1 to 1000 symbols')
-
-export const inputPostBlogValidator = body('content')
-.isString()
-.trim()
-.notEmpty()
-.custom(async (id) => {
+export const inputPostBlogValidator = body("blogId")
+  .isString()
+  .trim()
+  .notEmpty()
+  .custom(async (id) => {
     const blogExist = await blogsRepositories.findBlogById(id);
     if (!blogExist) {
       throw new Error("Blog is not exists");
     }
     return true;
-  }).withMessage('Descriptionme should be length from 1 to 100 symbols')
-  
+  })
+  .withMessage("Invalid blogId");
