3c1f097d2f276d5e8217281d1a9328521d2e2b22
MihPar <9496260@gmail.com>
2023-10-12 11:01:39 +0300

    1210231101

--- src/domain/userService.ts
+++ src/domain/userService.ts
@@ -4,19 +4,6 @@ import bcrypt from 'bcrypt'
 import { ObjectId } from 'mongodb';
 
 export const userService = {
-	async checkCridential(loginOrEmail: string, password: string) {
-		const user: any = await userRepositories.findByLoginOrEmail(loginOrEmail)
-		if(!user) return false
-		const passwordHash = await this._generateHash(password, user.passwordSalt)
-		if(user.passwordHash !== passwordHash) {
-			return false
-		}
-		return true
-	},
-	async _generateHash(password: string, salt: string) {
-		const hash = await bcrypt.hash(password, salt)
-		return hash
-	},
 	async createNewUser(login: string, password: string, email: string): Promise<UserType> {
 		const passwordSalt = await bcrypt.genSalt(10)
 		const passwordHash = await this._generateHash(password, passwordSalt)
@@ -31,6 +18,20 @@ export const userService = {
 		}
 		return userRepositories.createUser(newUser)
 	},
+	async checkCridential(loginOrEmail: string, password: string) {
+		const user: any = await userRepositories.findByLoginOrEmail(loginOrEmail)
+		if(!user) return false
+		const passwordHash = await this._generateHash(password, user.passwordSalt)
+		if(user.passwordHash !== passwordHash) {
+			return false
+		}
+		return true
+	},
+	async _generateHash(password: string, salt: string) {
+		const hash = await bcrypt.hash(password, salt)
+		return hash
+	},
+	
 	async deleteUserId(id: string): Promise<boolean> {
 		const deleteId = await userRepositories.deleteById(id)
 		return deleteId
