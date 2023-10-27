4303b3f4466472d2094a988aa7c7aa842ec806be
MihPar <9496260@gmail.com>
2023-10-27 11:31:40 +0300

    2710231131

--- src/manager/email-manager.ts
+++ src/manager/email-manager.ts
@@ -4,5 +4,6 @@ import { emailAdapter } from "../adapter/email-adapter"
 export const emailManager = {
 	async sendEamilConfirmationMessage(user: DBUserType): Promise<void> {
 		const result = await emailAdapter.sendEmail(user.accountData.email, user.emailConfirmation.confirmationCode)
+		return result
 	}
 }
